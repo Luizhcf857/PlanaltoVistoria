bb60322cc9140a8baac1260c1bc6c3aa57171b51
Luiz Henrique <luizhenrique3781@gmail.com>
2025-10-08 21:37:07 -0300

    Add files via upload

---
+++ ProjetoFinal/src/dao/VeiculoDAO.java
@@ -0,0 +1,154 @@
+package dao;
+
+import model.Veiculo;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import db.Conexao;
+import model.Cliente;
+
+public class VeiculoDAO {
+	public static void inserir(Veiculo veiculo) {
+		String sql = "INSERT INTO Veiculos (Placa, Marca, Modelo, Ano, Numero_chassi, Id_Clientes) VALUES (?, ?, ?, ?, ?, ?)";
+		try (Connection conn = Conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+			stmt.setString(1, veiculo.getPlaca());
+			stmt.setString(2, veiculo.getMarca());
+			stmt.setString(3, veiculo.getModelo());
+			stmt.setString(4, veiculo.getAno()); 
+			stmt.setString(5, veiculo.getNumero_chassi());
+			stmt.setInt(6, veiculo.getCliente().getId_Cliente());
+			stmt.executeUpdate();
+			System.out.println("Veículo cadastrado com sucesso!");
+			
+			try(ResultSet rs = stmt.getGeneratedKeys()){
+				
+				if(rs.next()) {
+					veiculo.setId_Veiculo(rs.getInt(1));
+					
+					System.out.println(veiculo.getId_Veiculo());
+				}
+				
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("Erro no cadastro do veículo: " + e.getMessage());
+		}
+	}
+
+	public List<Veiculo> listar() {
+		List<Veiculo> lista = new ArrayList<>();
+		String sql = "SELECT * FROM Veiculo";
+		try (Connection conn = Conexao.conectar();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql)) {
+			while (rs.next()) {
+				Veiculo veiculo = new Veiculo();
+				veiculo.setId_Veiculo(rs.getInt("Id_Veiculo"));
+				veiculo.setPlaca(rs.getString("Placa"));
+				veiculo.setMarca(rs.getString("Marca"));
+				veiculo.setModelo(rs.getString("Modelo"));
+				veiculo.setAno(rs.getString("Ano"));
+				veiculo.setNumero_chassi(rs.getString("Numero_chassi"));
+				//veiculo.setId_Cliente(rs.getInt("Id_Cliente"));
+				lista.add(veiculo);
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro ao listar veículos: " + e.getMessage());
+		}
+		return lista;
+	}
+
+	public void atualizar(Veiculo veiculo) {
+		String sql = "UPDATE Veiculo SET Placa = ?, Marca = ?, Modelo = ?, Ano = ?, Numero_chassi = ?, Id_Cliente = ? WHERE Id_Veiculo = ?";
+		try (Connection conn = Conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, veiculo.getPlaca());
+			stmt.setString(2, veiculo.getMarca());
+			stmt.setString(3, veiculo.getModelo());
+			stmt.setString(4, veiculo.getAno());
+			stmt.setString(5, veiculo.getNumero_chassi());
+			//stmt.setInt(6, veiculo.getId_Cliente());
+			stmt.setInt(7, veiculo.getId_Veiculo());
+			stmt.executeUpdate();
+			System.out.println("Veículo atualizado com sucesso!");
+		} catch (SQLException e) {
+			System.out.println("Erro ao atualizar veículo: " + e.getMessage());
+		}
+	}
+
+	public void excluir(int Id_Veiculo) {
+		String sql = "DELETE FROM Veiculo WHERE Id_Veiculo = ?";
+		try (Connection conn = Conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setInt(1, Id_Veiculo);
+			stmt.executeUpdate();
+			System.out.println("Veículo excluído com sucesso!");
+		} catch (SQLException e) {
+			System.out.println("Erro ao excluir veículo: " + e.getMessage());
+		}
+	}
+
+	public Veiculo buscarPorPlaca(String placa) {
+		String sql = "SELECT * FROM Veiculo WHERE Placa = ?";
+		try (Connection conn = Conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, placa);
+			try (ResultSet rs = stmt.executeQuery()) {
+				if (rs.next()) {
+					Veiculo veiculo = new Veiculo();
+					veiculo.setId_Veiculo(rs.getInt("Id_Veiculo"));
+					veiculo.setPlaca(rs.getString("Placa"));
+					veiculo.setMarca(rs.getString("Marca"));
+					veiculo.setModelo(rs.getString("Modelo"));
+					veiculo.setAno(rs.getString("Ano"));
+					veiculo.setNumero_chassi(rs.getString("Numero_chassi"));
+					
+					return veiculo;
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro ao buscar veículo por placa: " + e.getMessage());
+		}
+		return null;
+	}
+
+	public Veiculo buscarPorChassi(String numeroChassi) {
+		String sql = "SELECT * FROM Veiculo WHERE Numero_chassi = ?";
+		try (Connection conn = Conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, numeroChassi);
+			try (ResultSet rs = stmt.executeQuery()) {
+				if (rs.next()) {
+					Veiculo veiculo = new Veiculo();
+					veiculo.setId_Veiculo(rs.getInt("Id_Veiculo"));
+					veiculo.setPlaca(rs.getString("Placa"));
+					veiculo.setMarca(rs.getString("Marca"));
+					veiculo.setModelo(rs.getString("Modelo"));
+					veiculo.setAno(rs.getString("Ano"));
+					veiculo.setNumero_chassi(rs.getString("Numero_chassi"));
+					//veiculo.setId_Cliente(rs.getInt("Id_Cliente"));
+					return veiculo;
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro ao buscar veículo por chassi: " + e.getMessage());
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		Veiculo veiculo = new Veiculo();
+		
+		Cliente cliente = new Cliente();
+		cliente.setId_Cliente(1);
+		
+		
+		veiculo.setAno("2022");
+
+		veiculo.setMarca("Chevrolet");
+		veiculo.setModelo("Camaro");		
+		veiculo.setPlaca("MVY1730");
+		veiculo.setNumero_chassi("1bdmx1hSBf3Jj3786");
+		
+		veiculo.setCliente(cliente);
+		
+		VeiculoDAO.inserir(veiculo);
+		
+	}
+}
---
+++ ProjetoFinal/src/view/TesteCartao.java
@@ -0,0 +1,58 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TesteCartao {
+
+	public static void main(String[] args) {
+		JFrame tela = new JFrame("teste cartao");
+		
+		tela.setDefaultCloseOperation(tela.EXIT_ON_CLOSE);
+		tela.setSize(1024, 640);
+		tela.setLayout(new BorderLayout());
+		
+		JPanel container = new JPanel();
+		container.setOpaque(false);
+		container.setLayout(new GridBagLayout());
+		
+		JPanel cartao = new JPanel();
+		cartao.setLayout(new BorderLayout());
+		
+		JLabel infoCartao = new JLabel("Cadastro de Cartão");
+		infoCartao.setFont(new Font("Arial", Font.PLAIN, 19));
+		
+		JPanel panelInfoCartao = new JPanel();
+		panelInfoCartao.setLayout(new FlowLayout(FlowLayout.LEFT));
+		panelInfoCartao.setBorder(BorderFactory.createEmptyBorder(10, 155, 0, 0));
+		panelInfoCartao.add(infoCartao);
+		
+		JPanel panelLabelNumCartao = new JPanel();
+		panelLabelNumCartao.setLayout(new FlowLayout());
+		panelLabelNumCartao.setBorder(BorderFactory.createEmptyBorder(30, 155, 0 ,0));
+		
+		JLabel labelNumCartao = new JLabel("Digite o numero do cartão");
+		panelLabelNumCartao.add(labelNumCartao);
+		
+		JPanel campoNumCartao = new JPanel();
+		campoNumCartao.setLayout(new BoxLayout(campoNumCartao, BoxLayout.Y_AXIS));
+		
+		
+		JTextField numCartao = new JTextField(15);
+		campoNumCartao.setAlignmentX(Component.LEFT_ALIGNMENT);
+		campoNumCartao.setMaximumSize(new Dimension(100, 35));
+		campoNumCartao.add(numCartao);
+		
+		cartao.add(panelInfoCartao, BorderLayout.NORTH);
+		cartao.add(panelLabelNumCartao, BorderLayout.WEST);
+		cartao.add(campoNumCartao, BorderLayout.SOUTH);
+		
+		
+		
+		tela.add(cartao);
+		
+		tela.setVisible(true);
+
+	}
+
+}
